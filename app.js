042c8100defd35860c6a054406538566ddfb51f7
Krystyna Malewski <krmalewski@gmail.com>
2016-09-28 15:04:47 -0400

    Get rid of the DOM

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-
-var locate = window.location
-document.pet.value = locate
-
-var text = document.pet.gender.value
-var text2 = document.pet.color.value
-
-function delineate(str) {
-  theLeft = str.indexOf("=") + 1;
-  theRight = str.lastIndexOf("&");
-  return(str.substring(theLeft, theRight));
-}
+console.log('connected');
 
 
-function delineate2(str)
-{
-point = str.lastIndexOf("=");
-return(str.substring(point+1,str.length));
-}
+  var locate = window.location.search
+
+
+  // var text = document.pet.gender.value
+  // var text2 = document.pet.color.value
+
+  function delineate(str) {
+    theLeft = str.indexOf("=") + 1;
+    theRight = str.lastIndexOf("&");
+    return(str.substring(theLeft, theRight));
+  }
+
+
+  function delineate2(str) {
+    point = str.lastIndexOf("=");
+    return(str.substring(point+1,str.length));
+  }
+
+  console.log("You adopted a " + delineate(locate) )
+
